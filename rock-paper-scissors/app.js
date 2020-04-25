937503df8c5024a91b212e8fd07c2d8786a80012
Pete <45820811+Lucjus@users.noreply.github.com>
2020-04-25 21:43:19 +0200

    Add files via upload

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -4,7 +4,7 @@ class Computer {
     }
 
     pickHand() {
-        const index = Math.floor(Math.random(game.arrayHands.length) * 1)
+        const index = Math.floor(Math.random(game.arrayHands.length) * 5)
         this.pickedHand = index;
     }
 
@@ -12,26 +12,22 @@ class Computer {
 }
 
 class Player {
-    constructor(pickedHand, kurwas) {
+    constructor(pickedHand, commandCover) {
         this.pickedHand = pickedHand;
-        this.kurwas = kurwas;
+        this.commandCover = commandCover
     }
-
     playerPick(e) {
-        this.pickedHand = e.target.dataset.id
-        this.kurwas = e.target
-
-        ai.pickHand()
         game.animation()
-        this.kurwas.parentElement.style.display = 'block'
-        this.kurwas.parentElement.classList.add('player-circle')
-        this.kurwas.parentElement.style.top = 50 + '%'
-        this.kurwas.parentElement.style.left = 10 + '%'
-        this.divus = document.createElement('div')
-        this.divus.classList.add('computer-circle')
-        game.actionSection.appendChild(this.divus)
-        console.log(this.kurwas.parentElement)
-
+        ai.pickHand()
+        this.pickedHand = e.target.dataset.id
+        this.commandCover = e.target
+        this.commandCover.parentElement.style.display = 'block'
+        this.commandCover.parentElement.classList.add('player-circle')
+        this.commandCover.parentElement.style.top = 50 + '%'
+        this.commandCover.parentElement.style.left = 10 + '%'
+        this.cmpAnimationDiv = document.createElement('div')
+        this.cmpAnimationDiv.classList.add('computer-circle')
+        game.actionSection.appendChild(this.cmpAnimationDiv)
     }
 }
 
@@ -41,79 +37,97 @@ const player = new Player()
 class Game {
     constructor() {
         this.eventFired = false;
-        this.divek = document.querySelectorAll('.action-section-duel-commands')
-        this.divekk = [...this.divek]
+        this.cmp = document.querySelector('.cmp')
+        this.computerSvg = document.querySelector('.computer-svg')
+        this.text = document.querySelector('.text-result')
+        this.playBtn = document.querySelector('.play-again')
+        this.result = document.querySelector('.number')
+        this.closeIcon = document.querySelector('.close-icon')
+        this.modal = document.querySelector('.modal')
+        this.rulesBox = document.querySelector('.rules-box')
+        this.actionButton = document.querySelector('.rules-btn')
         this.actionSection = document.querySelector('#action-section')
-        this.result = document.querySelector('.okej')
-        this.hands = document.querySelectorAll('.kurwa')
+        this.duelCommandsNode = document.querySelectorAll('.action-section-duel-commands')
+        this.picksText = document.querySelectorAll('h1')
+        this.hands = document.querySelectorAll('.cover-command')
+        this.closeIcon.addEventListener('click', this.closeRulesBox)
+        this.actionButton.addEventListener('click', this.showRulesBox)
+        this.playBtn.addEventListener('click', this.reset)
+        this.duelCommandsArray = [...this.duelCommandsNode]
         this.arrayHands = [...this.hands]
         this.arrayHands.forEach(hand => hand.addEventListener('click', player.playerPick))
         this.arrayHands.forEach(hand => hand.addEventListener('click', this.check))
-        this.picksText = document.querySelectorAll('h1')
-
+        this.winDiv = document.createElement('div')
     }
     check() {
+        let d = (5 + ai.pickedHand - this.pickedHand) % 5
         setTimeout(function () {
             if (!game.eventFired) {
-                let winDiv = document.createElement('div')
-                let fiut = game.divek[ai.pickedHand];
-                fiut.style.display = 'block'
-                fiut.style.top = 50 + "%"
-                fiut.style.left = 64 + "%"
-                fiut.style.transform = "translateY(-50%)"
-
-                console.log(fiut)
-                if (this.pickedHand == 0 && ai.pickedHand == 1) {
-                    console.log('Miałeś nożyce, a komp papier, wygrałeś')
-                    winDiv.classList.add('wygrana')
-                    game.actionSection.appendChild(winDiv)
-                    game.result.textContent++
-                } else if (this.pickedHand == 0 && ai.pickedHand == 3) {
-                    game.result.textContent++
-                    console.log('Miałeś nożyce, a komp lizarda, wygrałeś ')
-                } else if (this.pickedHand == 1 && ai.pickedHand == 2) {
-                    game.result.textContent++
-                    console.log('Miałeś papier, a komp kamień, wygrałeś ')
-                } else if (this.pickedHand == 1 && ai.pickedHand == 4) {
-                    game.result.textContent++
-                    console.log('Miałeś papier, a komp spocka, wygrałeś ')
-                } else if (this.pickedHand == 2 && ai.pickedHand == 0) {
-                    console.log('Miałeś kamień, a komp nożyce, wygrałeś ')
-                    game.result.textContent++
-                } else if (this.pickedHand == 2 && ai.pickedHand == 3) {
-                    console.log('Miałeś kamień, a komp lizarda, wygrałeś ')
-                    game.result.textContent++
-                } else if (this.pickedHand == 3 && ai.pickedHand == 4) {
-                    console.log('Miałeś lizarda, a komp spocka, wygrałeś ')
-                    game.result.textContent++
-                } else if (this.pickedHand == 3 && ai.pickedHand == 1) {
-                    console.log('Miałeś lizarda, a komp papier, wygrałeś ')
-                    game.result.textContent++
-                } else if (this.pickedHand == 4 && ai.pickedHand == 0) {
-                    console.log('Miałeś spocka, a komp nożyce, wygrałeś ')
-                    game.result.textContent++
-                } else if (this.pickedHand == 4 && ai.pickedHand == 1) {
-                    console.log('Miałeś spocka, a komp kamień, wygrałeś ')
-                    game.result.textContent++
-                } else if (this.pickedHand == ai.pickedHand) {
-                    console.log('remis ')
-
+                game.text.classList.add('display-text')
+                game.playBtn.style.display = 'block'
+                game.cmp.style.display = 'block'
+                game.cmp.style.left = 64 + "%"
+                if (ai.pickedHand == 1) {
+                    game.cmp.classList.add('paper-command')
+                    game.computerSvg.classList.add('paper-svg')
+                } else if (ai.pickedHand == 0) {
+                    game.cmp.classList.add('scissors-command')
+                    game.computerSvg.classList.add('scissors-svg')
+                } else if (ai.pickedHand == 2) {
+                    game.cmp.classList.add('rock-command')
+                    game.computerSvg.classList.add('rock-svg')
+                } else if (ai.pickedHand == 3) {
+                    game.cmp.classList.add('lizard-command')
+                    game.computerSvg.classList.add('lizard-svg')
+                } else if (ai.pickedHand == 4) {
+                    game.cmp.classList.add('spock-command')
+                    game.computerSvg.classList.add('spock-svg')
                 }
+
+                if (d == 1 || d == 3) {
+                    game.text.textContent = 'YOU WIN'
+                    game.winDiv.classList.add('player-win')
+                    game.actionSection.appendChild(game.winDiv)
+                    game.result.textContent++
+                } else if (d == 2 || d == 4) {
+                    game.winDiv.classList.add('computer-win')
+                    game.actionSection.appendChild(game.winDiv)
+                    game.text.textContent = 'YOU LOSE'
+                } else if (d == 0) {
+                    game.text.textContent = 'TIE'
+                }
+
             }
         }.bind(this), 2000);
 
 
     }
 
-
-
     animation() {
-        this.divekk.forEach(hand => hand.style.display = 'none')
-        this.actionSection.style.backgroundImage = 'none'
-        this.arrayHands.forEach(hand => hand.style.display = 'none')
-        this.picksText.forEach(text => text.style.display = 'block')
+        game.actionSection.style.backgroundImage = 'none'
+        game.duelCommandsArray.forEach(hand => hand.style.display = 'none')
+        game.arrayHands.forEach(hand => hand.style.display = 'none')
+        game.picksText.forEach(text => text.style.display = 'block')
+    }
+    reset() {
+        game.text.textContent = ''
+        game.actionSection.style.backgroundImage = 'block'
+        game.cmp.style.display = 'none';
+        game.winDiv.style.display = 'none'
+        game.duelCommandsArray.forEach(hand => hand.style.display = 'block')
+        game.arrayHands.forEach(hand => hand.style.display = 'block')
+        game.picksText.forEach(text => text.style.display = 'none')
     }
 
+    showRulesBox() {
+        game.rulesBox.style.display = 'flex'
+        game.modal.style.display = 'block'
+    }
 
+    closeRulesBox() {
+        game.rulesBox.style.display = 'none'
+        game.modal.style.display = 'none'
+    }
 }
+
 const game = new Game();
